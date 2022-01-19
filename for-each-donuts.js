9781232ebf89808961988ca1189433caa5b99354
Ryan MacEachern <ryan.maceachern@gmail.com>
2022-01-19 17:33:30 -0600

    More advanced object manipulation

--- for-each-donuts.js
+++ for-each-donuts.js
@@ -9,5 +9,5 @@ var donuts = [
 /* Under the console.log portion of this function, it seemed at first that I should call the method with "donuts.type" (plural donuts, after the name of the array). However, since we are already calling the donuts array with the donuts.forEach() command, we now need to reference .type and .cost by "donut.type" and "donut.cost" (singular donut)respectively. This is because "donut" now refers to each element within the array "donuts" we have already called on. */
 
 donuts.forEach(function(donut) {
-  console.log(donut.type + " donuts cost $" + donut.cost + " each.");
+  console.log(donut.type + " donuts cost $" + donut.cost + " each");
 });
